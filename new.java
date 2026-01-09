356573c9115cd18b7dbaeaa8cf736a343ddc4987
Kartvyagupta19 <kartvyagupta3@gmail.com>
2026-01-09 11:53:01 +0530

    new java

---
+++ new.java
@@ -0,0 +1,18 @@
+public class Demo1{
+	static int x=10;
+		int l=9;
+		
+		public static void m1() {
+		int j=6;}
+	
+	public static void main(String[] args){
+		int x=11;
+		int l=8;
+	System.out.println("hey");
+	System.out.println("main static vary" +x);
+	System.out.println("main static vary" +l);
+	
+		}
+}
+
+	
