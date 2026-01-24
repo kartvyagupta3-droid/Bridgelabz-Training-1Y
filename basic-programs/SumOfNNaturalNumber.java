cff60da609798c340b97b55ba262e52600567c9d
Kartvyagupta19 <kartvyagupta3@gmail.com>
2026-01-25 00:48:07 +0530

    [kartvya] Sum of n natural number ?

---
+++ basic-programs/SumOfNNaturalNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ public class SumOfNNaturalNumber{
+  public static void main(String[] args){
+   
+  Scanner sc=new Scanner(System.in);
+  int num=sc.nextInt();
+  
+  if(num>=0){
+  int sum=num(num+1)/2;
+  System.out.println("The sum of" +num+ "natural numbers is" +sum);
+  else
+  System.out.println("The sum of" +num+ "is not natural numbers);
+  }
+  }
+  }
