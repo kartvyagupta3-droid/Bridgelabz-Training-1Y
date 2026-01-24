ae7f70973a6d7df2e9bac6fdd8d95ff367639445
Kartvyagupta19 <kartvyagupta3@gmail.com>
2026-01-25 01:48:01 +0530

    [kartvya] sum until user enter zero or negative

---
+++ basic-programs/SumUntilZeroNegative.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ public class SumUntilZeroNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double total = 0.0;  
+		
+        while(true){  
+            double num=sc.nextDouble();
+            if (number<=0) {
+                break;}
+            total+=num;
+        }
+        System.out.println("Total sum=" +total);
+    }
+}
