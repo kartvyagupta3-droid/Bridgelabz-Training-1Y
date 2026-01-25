60f05b0d30241446a49aa6978f1b0ddd374280f9
Kartvyagupta19 <kartvyagupta3@gmail.com>
2026-01-26 01:03:51 +0530

    [kartvya] power of a number

---
+++ basic-programs/PowerOfNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ public class PowerOfNumber{
+  public static void main(String[] args){
+   
+   Scanner sc=new Scanner(System.in);
+   int num=sc.nextInt();
+   int pow=sc.nextInt();'
+   int res=1;
+   
+   if(num>=0){
+   for(int i=i; i<pow; i++){
+   res=res*num;}
+   }
+   System.out.println(res);
+   }
+   }
+   
