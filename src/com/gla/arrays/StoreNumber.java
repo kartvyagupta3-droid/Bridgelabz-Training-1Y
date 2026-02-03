ddcc155159b528c3b184b02ab10e16eada905d3d
Kartvyagupta19 <kartvyagupta3@gmail.com>
2026-02-03 20:48:37 +0530

    [kartvya]StoreNumber

---
+++ src/com/gla/arrays/StoreNumber.java
@@ -0,0 +1,35 @@
+package com.gla.arrays;
+import java.util.Scanner;
+ public class StoreNumber {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double[] numbers = new double[10];
+
+        double total = 0.0;
+        int index = 0;
+
+        while (true) {
+            System.out.print("Enter a number (0 or negative to stop): ");
+            double num = sc.nextDouble();
+              if (num <= 0) {
+                  break;
+                    }
+              if (index == 10) {
+                  System.out.println("Array is full (max 10 numbers).");
+                  break;
+                    }
+              numbers[index] = num;
+              index++;
+                }
+        for (int i = 0; i < index; i++) {
+            total += numbers[i];
+        }
+        System.out.println("\nNumbers entered:");
+          for (int i = 0; i < index; i++) {
+              System.out.println(numbers[i]);
+                }
+                System.out.println("Sum of all numbers = " + total);
+            }
+        }
+
+
